b20d4f7bd04f734716abcd397643d53012ce5535
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-08-09 22:28:50 -0300

    ordenação

--- tst-eda/InsertionRecursivo.java
+++ tst-eda/InsertionRecursivo.java
@@ -1,13 +1,29 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class InserePrimeiro {
+class InsertionRecursivo {
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int[] numeros = transformaLista(sc.nextLine().split(" "));
-    inserePrimeiro(numeros);
+    insertionSortRecursivo(numeros, 1, numeros.length-1);
     System.out.println(Arrays.toString(numeros));
 
+  }
+
+  private static void insertionSortRecursivo(int[] numeros, int i, int n) {
+    int j = i;
+    while (j > 0 && numeros[j] < numeros[j-1]) {
+      int aux = numeros[j];
+      numeros[j] = numeros[j - 1];
+      numeros[j - 1] = aux;
+      j -= 1;
+    }
+    if (i + 1 <= n) {
+      System.out.println(Arrays.toString(numeros));
+      insertionSortRecursivo(numeros, i + 1, n);
+  }
+    
   }
 
   public static int[] transformaLista(String[] lista){
@@ -16,18 +32,5 @@ class InserePrimeiro {
       retorno[i] = Integer.parseInt(lista[i]);
     }
     return retorno;
-  }
-
-  public static void inserePrimeiro(int[] numeros){
-    for (int i = 1; i < numeros.length; i++) { 
-      int j = i;
-      while (j > 0 && numeros[j] < numeros[j-1]) {
-        int aux = numeros[j];
-        numeros[j] = numeros[j - 1];
-        numeros[j - 1] = aux;
-        j -= 1;
-      }
-    
-    }	
   }
 }
