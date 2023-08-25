0ee30215cad614f84c734b353ba487ae960cdc7b
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-08-25 11:44:44 -0300

    counting

--- roteiros-leda/R04-01-Rot-LinearSorting-Countingsort/src/main/java/sorting/linearSorting/CountingSort.java
+++ roteiros-leda/R04-01-Rot-LinearSorting-Countingsort/src/main/java/sorting/linearSorting/CountingSort.java
@@ -17,8 +17,54 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		boolean checkParameters = array.length > 0 && rightIndex <= array.length-1 && leftIndex >= 0;
+		if(checkParameters){
+			int k = findBigger(array, leftIndex, rightIndex);
+			Integer[] C = frequency(array, leftIndex, rightIndex, k);
+			cumulative(C);
+			Integer[] B = sortingIntegers(array, C, rightIndex);
+			changeArray(B, array);
+		}
+	}
+
+	private static int findBigger(Integer[] array, int leftIndex, int rightIndex){
+		Integer bigger = array[leftIndex];
+		for (int i = leftIndex+1; i <= rightIndex; i++) {
+			if(bigger.compareTo(array[i]) < 0){
+				bigger = array[i];
+			}
+		}
+		return bigger;
+	}
+
+	private Integer[] frequency(Integer[] array, int leftIndex, int rightIndex, int k){
+		// null to zero
+		Integer[] C = new Integer[k+1];
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			C[array[i]] += 1;	
+		}
+		return C;
+	}
+
+	private void cumulative(Integer[] C){
+		for (int i = 1; i < C.length; i++) {
+			C[i] += C[i-1];
+		}
+	}
+
+	private Integer[] sortingIntegers(Integer[] array, Integer[] C, int rightIndex){
+		// de tras pra frente
+		Integer[] B = new Integer[rightIndex+1];
+		for (int i = 0; i < rightIndex; i++) {
+			B[C[array[i]-1]] = array[i];
+		}
+		return B;
+	}
+
+	private void changeArray(Integer[] B, Integer[] array){
+		for (int i = 0; i < B.length; i++) {
+			array[i] = B[i];
+		}
 	}
 
 }
