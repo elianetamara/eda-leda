70a73b5fa734d0cdcf4cf4b3c37d02cc6a5cc838
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-10-07 18:00:00 -0300

    feat: SimpleBSTManipulationImpl

--- roteiros-leda/R11-01-Rot-BST/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
+++ roteiros-leda/R11-01-Rot-BST/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
@@ -1,7 +1,10 @@
 package adt.bst;
 
+import adt.bt.BTNode;
+
 /**
- * - Esta eh a unica classe que pode ser modificada 
+ * - Esta eh a unica classe que pode ser modificada
+ * 
  * @author adalbertocajueiro
  *
  * @param <T>
@@ -10,20 +13,63 @@ public class SimpleBSTManipulationImpl<T extends Comparable<T>> implements Simpl
 
 	@Override
 	public boolean equals(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return equals((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
+	}
+
+	private boolean equals(BSTNode<T> node1, BSTNode<T> node2) {
+		boolean equals = false;
+		if (node1.isEmpty() && node2.isEmpty()) {
+			equals = true;
+		} else if (!node1.isEmpty() & !node2.isEmpty()) {
+			if (node1.equals(node2)) {
+				equals = equals((BSTNode<T>) node1.getLeft(), (BSTNode<T>) node2.getLeft())
+						&& equals((BSTNode<T>) node1.getRight(), (BSTNode<T>) node2.getRight());
+			}
+		}
+
+		return equals;
 	}
 
 	@Override
 	public boolean isSimilar(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return isSimilar((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
+	}
+
+	private boolean isSimilar(BSTNode<T> node1, BSTNode<T> node2) {
+		boolean similar = false;
+		if (node1.isEmpty() && node2.isEmpty()) {
+			similar = true;
+		} else if (!node1.isEmpty() && !node2.isEmpty()) {
+			similar = isSimilar((BSTNode<T>) node1.getLeft(), (BSTNode<T>) node2.getLeft())
+					&& isSimilar((BSTNode<T>) node1.getRight(), (BSTNode<T>) node2.getRight());
+		}
+		return similar;
 	}
 
 	@Override
 	public T orderStatistic(BST<T> tree, int k) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		T order = null;
+		int size = tree.size();
+		if (k >= 1 && k <= size) {
+			if (k == 1) {
+				order = tree.minimum().getData();
+			} else if (k == size) {
+				order = tree.maximum().getData();
+			} else {
+				order = orderStatistic(tree, tree.minimum(), k);
+			}
+		}
+		return order;
+	}
+
+	private T orderStatistic(BST<T> tree, BSTNode<T> node, int k) {
+		T order = null;
+		if (k == 1) {
+			order = node.getData();
+		} else {
+			order = orderStatistic(tree, tree.sucessor(node.getData()), --k);
+		}
+		return order;
 	}
 
 }
