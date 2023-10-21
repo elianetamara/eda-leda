a1db73620a242f9fa68bde13b35ff3ed7f77a5dc
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2023-10-21 14:55:21 -0300

    feat: AVLVerifier & AVLCountAndFill

--- roteiros-leda/R13-01-Rot-AVL/src/main/java/adt/avltree/AVLCountAndFillImpl.java
+++ roteiros-leda/R13-01-Rot-AVL/src/main/java/adt/avltree/AVLCountAndFillImpl.java
@@ -1,5 +1,10 @@
 package adt.avltree;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import adt.bst.BSTNode;
+
 public class AVLCountAndFillImpl<T extends Comparable<T>> extends
 		AVLTreeImpl<T> implements AVLCountAndFill<T> {
 
@@ -9,7 +14,7 @@ public class AVLCountAndFillImpl<T extends Comparable<T>> extends
 	private int RLcounter;
 
 	public AVLCountAndFillImpl() {
-		
+
 	}
 
 	@Override
@@ -34,8 +39,43 @@ public class AVLCountAndFillImpl<T extends Comparable<T>> extends
 
 	@Override
 	public void fillWithoutRebalance(T[] array) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		HashSet<T> set = new HashSet<>();
+		T[] bstArray = order();
+
+		for (int i = 0; i < bstArray.length; i++) {
+			set.add(bstArray[i]);
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			set.add(array[i]);
+		}
+
+		array = (T[]) set.toArray(new Comparable[0]);
+		Arrays.sort(array);
+
+		this.root = new BSTNode<>();
+
+		int level = 0;
+		while (fillWithoutRebalance(0, array.length, level, array)) {
+			level++;
+		}
+	}
+
+	private boolean fillWithoutRebalance(int left, int right, int level, T[] array) {
+		boolean filled = false;
+
+		if (right > left) {
+			int middle = left + (right - left) / 2;
+			if (level == 0) {
+				insert(array[middle]);
+				filled = true;
+			} else {
+				filled = fillWithoutRebalance(left, middle, level - 1, array);
+				filled = fillWithoutRebalance(middle + 1, right, level - 1, array);
+			}
+		}
+
+		return filled;
 	}
 
 }
